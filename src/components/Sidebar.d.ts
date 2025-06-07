b2184d0ae99d4091002b3be232a550a25bb0b23b
Rameezrica <rxeza100@gmail.com>
2025-06-07 19:14:58 +0530

    Run Knip to clean up unused files

--- src/components/Sidebar.d.ts
+++
@@ -1,5 +0,0 @@
-declare module './Sidebar' {
-  import * as React from 'react';
-  const Sidebar: React.FC<React.PropsWithChildren<{ className?: string; style?: React.CSSProperties }>>;
-  export default Sidebar;
-} 
--- src/components/ui/Card.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Card: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <div className="bg-card rounded-lg shadow border border-border overflow-hidden">{children}</div>
-); 
